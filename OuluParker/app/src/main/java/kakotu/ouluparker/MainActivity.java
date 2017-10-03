42dfa35474773af075f613eb2e118e73e0224af0
Janne Turunen <t5tuja02@students.oamk.fi>
2017-10-03 08:33:23 +0300

    First Commit

---
+++ OuluParker/app/src/main/java/kakotu/ouluparker/MainActivity.java
@@ -0,0 +1,13 @@
+package kakotu.ouluparker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
